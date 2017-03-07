fca5432a7c770d4d7b8aba011b845738effe7073
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-07 14:44:57 +0100

    Fix jpeg typo in test

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,10 +4,10 @@ feature 'Creating posts' do
 	scenario 'can create a post' do
 		visit '/'
 		click_link 'New Post'
-		attach_file('Image', "spec/files/images/coffee.jpg")
+		attach_file('Image', "spec/files/images/coffee.jpeg")
 		fill_in 'Caption', with: "nom nom nom #coffeetime"
 		click_button 'Create Post'
 		expect(page).to have_content("#coffeetime")
-		expect(page).to have_css("img[src*='coffee.jpg']")
+		expect(page).to have_css("img[src*='coffee.jpeg']")
 	end
 end
