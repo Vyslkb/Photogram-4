abc0a5707627b0810504577ce9fefdf40a4bafc3
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-13 16:03:12 +0100

    Add space

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -7,6 +7,7 @@ feature 'Editing posts' do
 		find(:xpath, "//a[contains(@href, 'posts/1')]").click
 		click_link "Edit Post"
 	end
+
 	scenario 'can edit a post' do
 		fill_in "Caption", with: "Oh my geeeerd"
 		click_button "Update Post"
@@ -14,4 +15,5 @@ feature 'Editing posts' do
 		expect(page).to have_content("Post Updated")
 		expect(page).to have_content("Oh my geeeerd")		
 	end
+	
 end
