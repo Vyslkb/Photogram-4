400c90f426fbf5bdf9903f070b0e365666bee350
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-14 15:04:25 +0100

    Add Post edit and update path in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,21 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			flash[:success] = "Post Updated"
+			redirect_to(post_path(@post))
+		else
+			flash.now[:alert] = "Update failed. Please check the form"
+			render :edit
+		end
+	end
+
 	private
 
 	def post_params
