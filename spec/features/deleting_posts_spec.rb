80648d66a5e91854580387c139f3f913db28579a
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-14 15:30:23 +0100

    Add delete posts spec

---
+++ spec/features/deleting_posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Deleting posts' do
+
+	background do
+		post = create(:post, caption: "Yolo!")
+		visit '/'
+		find(:xpath, "//a[contains(@href, 'posts/1')]").click
+		click_link "Edit Post"
+	end
+
+	scenario 'can delete a post' do
+		click_link "Delete Post"
+		expect(page).to have_content("Post has been deleted")
+		expect(page).to_not have_content("Yolo!")
+	end
+
+end
