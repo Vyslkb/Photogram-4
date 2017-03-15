16a04fe768deb45b72c5b3ed51d379c50a413995
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-15 13:17:35 +0100

    Add destroy method in controller with flash confirmation message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,13 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		flash[:success] = "Post has been deleted."
+		redirect_to posts_path
+	end
+
 	private
 
 	def post_params
