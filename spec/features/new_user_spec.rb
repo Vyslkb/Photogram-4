696ce0ed7d6dc94265a50d9e519a9080a691e642
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-21 19:32:55 +0100

    Add tests that should have been added earlier and not later..

--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -10,12 +10,40 @@ feature "Creating a new user" do
 	scenario "can create a new user via the index page" do
 		fill_in "User name", with: "Johnny Bravo"
 		fill_in "Email", with: "johnny@bravo.com"
-		fill_in "Password", with: "supersecret"
-		fill_in "Password Confirmation", with: "supersecret", match: :first
-		click_button "Sign Up"
+		fill_in "Password", with: "supersecret", match: :first
+		fill_in "Password confirmation", with: "supersecret"
+		click_button "Sign up"
 		expect(page).to have_content("Welcome! You have signed up successfully.")
 	end
+	
+	scenario 'requires a user name to successfully create an account' do
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :first
+    fill_in 'Password confirmation', with: 'supersecret'
 
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
+  end
+
+	scenario 'requires a user name to be more than 4 characters' do
+    fill_in 'User name', with: 'h'
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :first
+    fill_in 'Password confirmation', with: 'supersecret'
+
+    click_button 'Sign up'
+    expect(page).to have_content('minimum is 4 characters')
+  end
+
+	scenario 'requires a user name to be less than 16 characters' do
+    fill_in 'User name', with: 'h' * 17
+    fill_in 'Email', with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password', with: 'supersecret', match: :first
+    fill_in 'Password confirmation', with: 'supersecret'
+
+    click_button 'Sign up'
+    expect(page).to have_content("maximum is 16 characters")
+  end
 
 end
 
