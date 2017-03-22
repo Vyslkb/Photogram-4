cee17ef11823abfb2145be9a6bcaa1b52d023ca4
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-22 11:38:39 +0100

    Add test for signing out

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User Authentication' do
 
 	background do
-		user = create(:user, email: "yolo@yolo.com", user_name: "yoloboi", password: "yolo_passoword" , id: 1)
+		user = create(:user)
 	end
 
 	scenario 'can log in from the index via the dynamic navbar' do
@@ -11,14 +11,23 @@ feature 'User Authentication' do
 		expect(page).to_not have_content("New Post")
 
 		click_link "Login"
-		fill_in "Email", with: "yolo@yolo.com"
-		fill_in "Password", with: "yolo_passoword"
+		fill_in "Email", with: "markym@ark.com"
+		fill_in "Password", with: "yolobro"
 		click_button "Log in"
 
 		expect(page).to have_content("Signed in successfully.")
 		expect(page).to_not have_content("Register")
 		expect(page).to have_content("Logout")
-
 	end  
+	
+	scenario 'can log out once logged in' do
+		visit "/"
+		click_link "Login"
+		fill_in "Email", with: "markym@ark.com"
+		fill_in "Password", with: "yolobro"
+		click_button "Log in"
+		click_link "Logout"
+		expect(page).to have_content("Signed out successfully.")
+	end
 
 end
