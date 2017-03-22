3d4dfb72494df20d7ef3752a4ddff4ab239cad73
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-22 15:01:34 +0100

    Add corrected migrations and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322115300) do
+ActiveRecord::Schema.define(version: 20170322121822) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170322115300) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.         "user"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
